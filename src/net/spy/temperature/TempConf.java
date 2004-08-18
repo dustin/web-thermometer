cbce914db6c2d545f1771753814c038ed7c861f5
Dustin Sallings <dustin@spy.net>
2004-08-18 01:15:31 +0000

    added wap report and config validation

---
+++ src/net/spy/temperature/TempConf.java
@@ -0,0 +1,22 @@
+// Copyright (c) 2000  SPY Internetworking <dustin@spy.net>
+
+package net.spy.temperature;
+
+import java.io.File;
+
+import net.spy.SpyConfig;
+
+public class TempConf extends SpyConfig {
+
+	private File configs[]={
+		new File("/afs/spy.net/misc/web/etc/temperature.conf"),
+		new File("/usr/local/etc/temperature.conf"),
+		new File("temperature.conf"),
+		new File("/tmp/temperature.conf")
+	};
+
+	public TempConf() {
+		super(); // Thanks for asking
+		loadConfig(configs);
+	}
+}
---
+++ src/net/spy/temperature/WapReport.java
@@ -0,0 +1,135 @@
+// Copyright (c) 2004  Dustin Sallings <dustin@spy.net>
+
+package net.spy.temperature;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import java.text.MessageFormat;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.spy.jwebkit.JWHttpServlet;
+
+import net.spy.temperature.sp.SummaryByDay;
+
+/**
+ * Servlet for displaying reports via WML.
+ */
+public class WapReport extends JWHttpServlet {
+
+	/**
+	 * Get an instance of WapReport.
+	 */
+	public WapReport() {
+		super();
+	}
+
+	private String getWml() throws ServletException {
+		Map stats=new HashMap();
+		Map nameMap=new HashMap();
+
+		try {
+			SummaryByDay sbd=new SummaryByDay(new TempConf());
+			ResultSet rs=sbd.executeQuery();
+			while(rs.next()) {
+				Stat s=new Stat(rs);
+				List l=(List)stats.get(s.sn);
+				if(l == null) {
+					l=new ArrayList();
+					stats.put(s.sn, l);
+				}
+				l.add(s);
+				nameMap.put(s.sn, s.name);
+			}
+			rs.close();
+			sbd.close();
+		} catch(SQLException e) {
+			throw new ServletException("Problem getting wml", e);
+		}
+
+		StringBuffer sb=new StringBuffer(8192);
+		sb.append("<?xml version=\"1.0\"?>\n");
+		sb.append("<!DOCTYPE wml PUBLIC ");
+		sb.append("\"-//WAPFORUM//DTD WML 1.1//EN\" ");
+		sb.append("\"http://www.wapforum.org/DTD/wml_1.1.xml\">\n");
+		sb.append("<wml>\n");
+		sb.append("<card id=\"home\" title=\"Therms\">\n");
+
+		for(Iterator i=nameMap.entrySet().iterator(); i.hasNext(); ) {
+			Map.Entry me=(Map.Entry)i.next();
+			sb.append("<a href=\"#" + me.getKey() + "\">"
+				+ me.getValue() + "</a><br/>\n");
+		}
+
+		sb.append("</card>\n");
+
+		MessageFormat mf=new MessageFormat("min={0,number,#.##} "
+			+ "max={1,number,#.##} "
+			+ "avg={2,number,#.##} "
+			+ "stddev={3,number,#.##}<br/>");
+
+		for(Iterator i=stats.entrySet().iterator(); i.hasNext(); ) {
+			Map.Entry me=(Map.Entry)i.next();
+			sb.append("<card id=\"" + me.getKey() + "\" title=\""
+				+ nameMap.get(me.getKey()) + "\">\n");
+
+
+			for(Iterator i2=((List)me.getValue()).iterator(); i2.hasNext(); ) {
+				Stat s=(Stat)i2.next();
+				sb.append("<p>" + s.day + "<br/>");
+				Object args[]={new Float(s.min), new Float(s.max),
+					new Float(s.avg), new Float(s.stddev)};
+				sb.append(mf.format(args));
+				sb.append("</p>");
+			}
+
+			sb.append("</card>\n");
+		}
+
+		sb.append("</wml>\n");
+		return(sb.toString());
+	}
+
+	/** 
+	 * Process this request.
+	 */
+	protected void doGetOrPost(HttpServletRequest req,
+		HttpServletResponse res) throws ServletException, IOException {
+
+		res.setContentType("text/vnd.wap.wml");
+		sendSimple(getWml(), res);
+	}
+
+	private static class Stat extends Object {
+		public String sn=null;
+		public String name=null;
+		public String day=null;
+		public float min=0.0f;
+		public float avg=0.0f;
+		public float stddev=0.0f;
+		public float max=0.0f;
+
+		public Stat(ResultSet rs) throws SQLException {
+			super();
+			sn=rs.getString("serial_num");
+			name=rs.getString("name");
+			day=rs.getString("day");
+			min=rs.getFloat("min_temp");
+			avg=rs.getFloat("avg_temp");
+			stddev=rs.getFloat("stddev_temp");
+			max=rs.getFloat("max_temp");
+		}
+	}
+
+}
