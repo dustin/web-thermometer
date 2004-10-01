2d9ef93faac67eb0fda31e0d834c004a78b84506
Dustin Sallings <dustin@spy.net>
2004-10-01 07:44:05 +0000

    Added daily rollups.

--- etc/report.sql
+++ etc/report.sql
@@ -46,3 +46,34 @@ insert into rollups_month
   having
     date(date_trunc('month', ts)) > (select max(month) from rollups_month)
 ;
+
+-- daily rollups
+create table rollups_day (
+	sensor_id integer not null,
+	min_reading float not null,
+	avg_reading float not null,
+	stddev_reading float not null,
+	max_reading float not null,
+	day date not null,
+	foreign key(sensor_id) references sensors(sensor_id)
+);
+create unique index rollups_d_sensts on rollups_day(sensor_id, day);
+create index rollups_day_byts on rollups_day(day);
+
+-- Update the daily rollups
+insert into rollups_day
+  select
+  	sensor_id,
+		min(sample) as min_reading,
+		avg(sample) as avg_reading,
+		stddev(sample) as stddev_reading,
+		max(sample) as max_reading,
+		date(ts) as day
+  from
+    samples
+  where
+  	ts > (select max(day) from rollups_day)
+	and ts < current_date
+  group by
+  	sensor_id, day
+;
